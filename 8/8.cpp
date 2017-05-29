cb5a9bea69cb82d9f20d476cef4c9241fc2c738a
Sharath Huddar <sharath.huddar@gmail.com>
2017-05-29 17:20:35 +0530

    Fix program 8

--- 8/8.cpp
+++ 8/8.cpp
@@ -4,116 +4,118 @@
  */
 
 
-#include <iostream>
-#include <string>
-#include <fstream>
+ #include <iostream>
+ #include <string>
+ #include <fstream>
+ #include <stdlib.h>
 
-using namespace std;
+ using namespace std;
 
-class coseq {
-public:
-string list[4][50];
-string olist[50];
-int c1[4], c2[4];
-void l_list();
-void r_file(int i);
-void s_list(int i);
-void merge();
-};
+ class coseq {
+ public:
+   string list[8][50];
+   string outlist[200];
+   int count1[8], count2[8];
+   void read_file(int i);
+   void sort_list(int i);
+   void kwaymerge();
+ };
 
-void coseq::r_file(int i){
-		fstream fp;
-		char name[100];
-		switch(i) {
-		case 1: fp.open("n1.txt", ios::in); break;
-		case 2: fp.open("n2.txt", ios::in); break;
-		case 3: fp.open("n3.txt", ios::in); break;
-				/*case 4: fp.open("n4.txt", ios::in); break;
-				   case 5: fp.open("n5.txt", ios::in); break;
-				   case 6: fp.open("n6.txt", ios::in); break;
-				   case 7: fp.open("n7.txt", ios::in); break;
-				   case 8: fp.open("n8.txt", ios::in); break;*/
-		}
-		while(!fp.eof()) {
-				fp.getline(name, 100, '\n');
-				list[i][++c1[i]] = name;
-		}
-		fp.close();
-}
+ void error(int);
 
-void coseq::l_list(){
-		for(int i=1; i<=3; i++) {
-				c1[i]=0;
-				r_file(i);
-				s_list(i);
-		}
-}
+ int main(){
+   coseq c;
+   for (int i = 0; i < 4; i++) {
+     c.count1[i] = 0;
+     c.read_file(i);
+     c.sort_list(i);
+   }
+   c.kwaymerge();
+   return 0;
+ }
 
-void coseq::s_list(int k){
-		int i, j;
-		string t;
-		for(i=1; i<=c1[k]; i++) {
-				for(j=i+1; j<=c1[k]; j++) {
-						if(list[k][i]>list[k][j]) {
-								t=list[k][i];
-								list[k][i]=list[k][j];
-								list[k][j]=t;
-						}
-				}
-		}
-}
+ void coseq::read_file(int i){
+   fstream fp;
+   string name;
+   switch (i) {
+     case 0: fp.open("n1.txt", ios::in); break;
+     case 1: fp.open("n2.txt", ios::in); break;
+     case 2: fp.open("n3.txt", ios::in); break;
+     case 3: fp.open("n4.txt", ios::in); break;
+   }
+   if (!fp) {
+     error(1);
+   }
+   while (fp) {
+     getline(fp, name);
+     if (name.length()>0) {
+       list[i][count1[i]++]=name;
+     }
+   }
+   fp.close();
+ }
 
-void coseq::merge(){
-		string sml;
-		int s_list, t=-1, strt=1, avail_list=3, avail[4],i;
-		for(i=1; i<=3; i++) {
-				avail[i]=1;
-				c2[i]=1;
-		}
-		while(avail_list > 1) {
-				if(!avail[strt]) {
-						strt++;
-						continue;
-				}
-				s_list = strt;
-				sml = list[strt][c2[strt]];
-				for(i=strt+1; i<=3; i++) {
-						if(!avail[i]) continue;
-						if(list[i][c2[i]]<sml) {
-								sml=list[i][c2[i]];
-								s_list=i;
-						}
-				}
-				c2[s_list]++;
-				if(c2[s_list]>c1[s_list]) {
-						avail[s_list]=0;
-						avail_list--;
-				}
-				olist[++t]=sml;
-				for(int j=1; j<=3; j++)
-						if(j!=s_list) {
-								if(list[j][c2[j]]==sml) {
-										c2[j]++;
-								}
-						}
-				for(i=1; i<=3; i++)
-						if(avail[i]) {
-								for(int j=c2[i]; j<=c1[i]; j++)
-										olist[++t]=list[i][j];
-						}
-		}
-		cout<<"\nMerged list ";
-		for(i=0; i<=t; i++) {
-				if(olist[i]==olist[i+1]) continue;
-				cout<<"\n"<<olist[i];
-		}
+ void coseq::sort_list(int k){
+   int i,j;
+   string temp;
+   for (i = 0; i < count1[k]; i++) {
+     for (j = i+1; j < count1[k]; j++) {
+       if (list[k][i]>list[k][j]) {
+         temp = list[k][i];
+         list[k][i] = list[k][j];
+         list[k][j] = temp;
+       }
+     }
+   }
+ }
 
-}
+ void coseq::kwaymerge(){
+   string sml;
+   int s_list, count3=0, strt=0, avail_list=4, avail[4],i;
+   for (i = 0; i < 4; i++) {
+     avail[i]=1;
+     count2[i]=0;
+   }
+   while (avail_list>1) {
+     if (!avail[strt]) {
+       strt++;
+       continue;
+     }
+     s_list=strt;
+     sml=list[strt][count2[strt]];
+     for (i = strt+1; i < 4; i++) {
+       if (!avail[i]) {
+         continue;
+       }
+       if (list[i][count2[i]]<sml) {
+         sml=list[i][count2[i]];
+         s_list=i;
+       }
+     }
+     count2[s_list]++;
+     if (count2[s_list]==count1[s_list]) {
+       avail[s_list]=0;
+       avail_list--;
+     }
+     outlist[count3++]=sml;
+   }
+   for (i = 0; i < 4; i++) {
+     if (avail[i]) {
+       for (int j = count2[i]; j < count1[i]; j++) {
+         outlist[count3++]=list[i][j];
+       }
+     }
+   }
+   cout<<"\nMerged list : \n";
+   for (i = 0; i < count3; i++) {
+     if (outlist[i]==outlist[i+1]) continue;
+       cout<<outlist[i]<<endl;
+   }
+ }
 
-
-int main() {
-		coseq c;
-		c.l_list();
-		c.merge();
-		return 0;
-}
+ void error(int error_type){
+   switch (error_type) {
+     case 1: cout<<"\nFATAL ERROR!: Unable to open the file(s)\n";
+             exit(0);
+   }
+ }
